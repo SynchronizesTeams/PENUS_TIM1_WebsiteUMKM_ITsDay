25ff5ae09aa4fff06b8ac6d7f7088bd492516f95
Kurnias <adityakurniasaputra903@gmail.com>
2025-06-26 23:42:11 +0700

    Fix Booking

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,28 +3,27 @@
    document.getElementById('appointmentForm').addEventListener('submit', function (e) {
     e.preventDefault(); 
 
-    const namaLengkap = document.getElementById('nama').value;
-    const noTelepon = document.getElementById('telepon').value;
+    const name = document.getElementById('nama').value;
+    const no_telp = document.getElementById('telepon').value;
     const email = document.getElementById('email').value;
-    const layanan = document.getElementById('selectedOption').innerText;
-    const tanggal = document.getElementById('date').value;
-    const pesan = document.getElementById('pesanTambahan').value;
+    const service = document.getElementById('selectedOption').innerText;
+    const date = document.getElementById('date').value;
+    const notes = document.getElementById('pesanTambahan').value;
 
-    if (layanan === 'Pilih layanan') {
+    if (service === 'Pilih layanan') {
       alert('Silakan pilih layanan terlebih dahulu');
       return;
     }
 
     const data = {
-      namaLengkap,
-      noTelepon,
+      name,
+      no_telp,
       email,
-      layanan,
-      tanggal,
-      pesan
+      service,
+      date,
+      notes
     };
 
-    
     fetch('https://api-salon.salondiantanjung.my.id/api/booking/send', {
       method: 'POST',
       headers: {
